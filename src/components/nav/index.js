23c17db95d9a91a004f07bfb7d0dd7e33ded30b1
djirdehh <hassan.djirdeh@gmail.com>
2020-04-03 22:15:02 -0400

    Edits

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -10,9 +10,14 @@ const Nav = () => {
         <Logo className={styles.logo} />
       </Link>
       <nav className={styles.links}>
+        <Link to="/faq" className={styles.link}>
+          FAQ
+        </Link>
         <Link to="/about" className={styles.link}>
           About
         </Link>
+      </nav>
+      <nav className={styles.links}>
         <Link to="/nominate" className={styles.link}>
           List your business
         </Link>
