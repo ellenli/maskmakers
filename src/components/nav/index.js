36b9dfa79189bceeac8952986e05a4f66570780f
djirdehh <hassan.djirdeh@gmail.com>
2020-04-05 15:48:09 -0400

    socials

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,9 +3,28 @@ import { Link } from "gatsby";
 import Logo from "../logo";
 import styles from "./nav.module.scss";
 
+import TwitterIcon from "../../icons/twitter";
+import InstagramIcon from "../../icons/instagram";
+
 const Nav = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.social}>
+        <a
+          href="https://twitter.com/maskmakersclub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterIcon />
+        </a>
+        <a
+          href="https://instagram.com/maskmakersclub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </a>
+      </div>
       <Link to="/">
         <Logo className={styles.logo} />
       </Link>
