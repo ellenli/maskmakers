67714245f5ee395639d5b8338d21479442477e50
Francine Navarro <francine.navarro@shopify.com>
2020-05-22 19:08:03 -0400

    Add styles for truncated descriptions

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import MapIcon from "../../icons/map";
 import styles from "./profile.module.scss";
 
+const MAX_DESCRIPTION_LENGTH = 80;
+
 function locationAndCountryText(location, country) {
   if (location && country) {
     return `${location}, ${country}`;
@@ -33,8 +35,24 @@ function locationAndCountry(location, country) {
   return null;
 }
 
+function truncateString(string) {
+  if (!string) {
+    return '';
+  }
+
+  const truncated = string.substr(0, MAX_DESCRIPTION_LENGTH);
+
+  return `${truncated}...`;
+} 
+
 const Profile = props => {
   const { image, name, location, country, websiteUrl } = props;
+
+  // CSS makes it easy to truncate a single line of text, but not multiple lines of text
+  const description = "Premium custom jewlery handcrafted with precision with some longer text that needs to be truncated because it is too long to show the whole thing";
+
+  const descriptionToDisplay = description.length > MAX_DESCRIPTION_LENGTH ? truncateString(description) : description;
+
 
   return (
     <a
@@ -48,6 +66,7 @@ const Profile = props => {
       <div className={styles.card}>
         <h2 className={styles.name}>{name}</h2>
         {locationAndCountry(location, country)}
+        <p className={styles.profileDescription}>{descriptionToDisplay}</p>
         <div className={styles.filterTags}>
           <div className={styles.giftCardBadge}>Gift card only</div>
           <div className={styles.filterTag}>Tag</div>
